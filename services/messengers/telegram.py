87841e4688ca6f20c0c1275c0446b91f8a1250d8
Bruxo <bruxo@cock.li>
2026-07-10 22:08:43 +0330

    this is the best version I've made.

--- services/messengers/telegram.py
+++
@@ -1,8 +0,0 @@
-from services.messengers.telegram_like import TelegramLikeAdapter
-
-
-class TelegramAdapter(TelegramLikeAdapter):
-    platform = "telegram"
-
-    def __init__(self, token: str, api_base_url: str = "https://api.telegram.org"):
-        super().__init__(token, api_base_url)
